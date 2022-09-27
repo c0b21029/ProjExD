d678ba5a45cdd07c4f4d1ab94aec93be587806c3
Masaki Otsu <c0b2102984@edu.teu.ac.jp>
2022-09-27 17:07:55 +0900

    修正

--- ex01/alphabet.py
+++ ex01/alphabet.py
@@ -1,24 +1,21 @@
 import random
 import time
 
-num_of_alphabet = 26  # 全アルファベット数
-num_of_all_chars = 10 # 対象文字数
-num_of_abs_chars = 2  # 欠損文字数
-num_of_trials = 2 # チャレンジできる回数
+num_of_alphabet = 26  
+num_of_all_chars = 10 
+num_of_abs_chars = 2  
+num_of_trials = 2 
 
 def shutudai(alphabet):
-    # 全アルファベットから，対象文字をランダムに10文字選ぶ
     all_chars = random.sample(alphabet, num_of_all_chars)
     print("対象文字：", end="")
     for c in sorted(all_chars): 
         print(c, end=" ")
     print()
-
-    # 対象10文字から，欠損文字をランダムに2文字選ぶ
     abs_chars = random.sample(all_chars, num_of_abs_chars)
     print("表示文字：", end="")
     for c in all_chars: 
-        if c not in abs_chars: # 欠損文字でなかったら表示
+        if c not in abs_chars: 
             print(c, end=" ")
     print()
     print("デバッグ用欠損文字：", abs_chars)
@@ -37,7 +34,7 @@ def kaito(seikai):
                 print("不正解です．またチャレンジしてください．")
                 return False
             else:
-                seikai.remove(c) # 正解した場合，その文字を正解リストから削除する（重複回答を防ぐため）
+                seikai.remove(c) 
         else:
             print("欠損文字も含めて完全正解です！！！")
             return True
