30ecccb06a3ebb7477f568608349c0d38787be8c
Masaki Otsu <c0b2102984@edu.teu.ac.jp>
2022-10-04 14:36:57 +0900

    ボタンに、クリックした際の反応を定義した

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,17 +1,23 @@
 import tkinter as tk
+import tkinter.messagebox as tkm
 
 root = tk.Tk()
 root.title("tk")
 root.geometry("300x500")
 
+def click_number(event):
+    btn = event.widget
+    num = int(btn["text"])
+    tkm.showinfo(f"{num}", f"[{num}]ボタンが押されました")
+
 r, c = 0, 0
 for i, num in enumerate(range(9,-1, -1), 1):
     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+    btn.bind("<1>", click_number)
     btn.grid(row=r, column=c)
     c += 1
     if i%3 == 0:
         r += 1
         c = 0
-
 
 root.mainloop()
