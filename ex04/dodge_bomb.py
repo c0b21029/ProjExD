af238ff30687f711e0364b5c149f5454d68051f9
Masaki Otsu <c0b2102984@edu.teu.ac.jp>
2022-10-25 15:38:46 +0900

    練習4-8

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -2,6 +2,14 @@ import pygame as pg
 import sys
 from random import randint
 
+def check_bound(obj_rct, scr_rct):
+    yoko, tate = +1, +1
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right: 
+        yoko = -1
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom: 
+        tate = -1
+    return yoko, tate
+
 def main():
     #練習1　　
     pg.display.set_caption("逃げろ！こうかとん") 
@@ -19,9 +27,14 @@ def main():
     #練習5
     bomb_sfc = pg.Surface((20, 20)) # 空のsurface
     pg.draw.circle(bomb_sfc, (255, 0, 0), (10, 10), 10) #円を描く
+    bomb_sfc.set_colorkey((0, 0, 0))
     bomb_rct  =bomb_sfc.get_rect()
     bomb_rct.centerx = randint(0, scrn_rct.width)
     bomb_rct.centery = randint(0,scrn_rct.height)
+
+    #練習6
+    vx, vy = +1, +1
+
 
 
     clock = pg.time.Clock() #練習1
@@ -38,9 +51,31 @@ def main():
         if key_states[pg.K_DOWN]: tori_rct.centery += 1  #こうかとんの縦座標+1
         if key_states[pg.K_LEFT]: tori_rct.centerx -= 1  #こうかとんの横座標-1
         if key_states[pg.K_RIGHT]: tori_rct.centerx += 1  #こうかとんの横座標+1
+        yoko, tate = check_bound(tori_rct, scrn_rct)
+        if yoko == -1:
+            if key_states[pg.K_LEFT]: 
+                tori_rct.centerx += 1
+            if key_states[pg.K_RIGHT]:
+                tori_rct.centerx -= 1
+        if tate == -1:
+            if key_states[pg.K_UP]: 
+                tori_rct.centery += 1  
+            if key_states[pg.K_DOWN]: 
+                tori_rct.centery -= 1
+        
+
         scrn_sfc.blit(tori_sfc, tori_rct) #練習3
 
-        scrn_sfc.blit(bomb_sfc, bomb_rct)
+
+        yoko, tate = check_bound(bomb_rct, scrn_rct)
+        vx *= yoko
+        vy *= tate
+        bomb_rct.move_ip(vx, vy)
+
+        scrn_sfc.blit(bomb_sfc, bomb_rct)#練習5
+
+        if tori_rct.colliderect(bomb_rct):
+            return 
 
         pg.display.update() #練習2
         clock.tick(1000) 
